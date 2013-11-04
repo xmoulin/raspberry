94d7c5cb0876ea4ec4f258c69c71c5b4db283030
xmoulin <xavier.moulin@gmail.com>
2013-11-04 22:09:49 +0100

    parametrabilite de la requete pour integration highchart

--- www/monitoring.php
+++ www/monitoring.php
@@ -5,8 +5,32 @@ $db = mysql_connect('localhost', 'root', '');
 // on sélectionne la base 
 mysql_select_db('monitoring',$db); 
 
-// on execute la requete
-$sth =  mysql_query("SELECT temperatureEau,temperature, humidity, date FROM indicateur");
+//Récuperation parametre de la requete
+if (isset($_GET['champ'])) {$champ = stripslashes(htmlspecialchars($_GET['champ']));}
+if (isset($_GET['limit'])) {$limit = (int)stripslashes(htmlspecialchars($_GET['limit']));}
+if (isset($_GET['offset'])) {$offset = (int)stripslashes(htmlspecialchars($_GET['offset']));}
+
+//Creation de la requete:
+$requete = "SELECT ";
+
+if (!empty($champ)) {
+	$requete = $requete . $champ;
+} else {
+	$requete = $requete . " temperatureEau,temperature, humidity, date ";
+}
+$requete = $requete . " FROM indicateur  Order by date ";
+
+//LIMIT 5 OFFSET 10;  # Retourne les enregistrements 6 à 15
+if (!empty($limit)) {
+	$requete = $requete . " LIMIT $limit ";
+}
+if (!empty($offset)) {
+	$requete = $requete . " OFFSET $offset ";
+}
+print($requete);
+
+//on execute la requete
+$sth =  mysql_query($requete);
 
 $rows = array();
 while($r = mysql_fetch_assoc($sth)) {
