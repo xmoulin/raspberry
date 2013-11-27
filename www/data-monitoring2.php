156d9554380111886beaf254d3c88c95a7afb4f0
xmoulin <xavier.moulin@gmail.com>
2013-11-27 15:09:14 +0100

    init multicourbes ajout du service datamonitoring2 pour tous indicateurs

---
+++ www/data-monitoring2.php
@@ -0,0 +1,60 @@
+<?php 
+// on se connecte à MySQL 
+$db = mysql_connect('localhost', 'root', ''); 
+
+// on sélectionne la base 
+mysql_select_db('monitoring',$db); 
+
+//Récuperation parametre de la requete
+if (isset($_GET['champ'])) {$champ = stripslashes(htmlspecialchars($_GET['champ']));}
+if (isset($_GET['champs'])) {$champs = stripslashes(htmlspecialchars($_GET['champs']));}
+if (isset($_GET['limit'])) {$limit = (int)stripslashes(htmlspecialchars($_GET['limit']));}
+if (isset($_GET['offset'])) {$offset = (int)stripslashes(htmlspecialchars($_GET['offset']));}
+
+//Creation de la requete:
+$requete = "SELECT ";
+
+if (!empty($champ)) {
+	$requete = $requete . $champ;
+} else if (!empty($champs)) {
+	$requete = $requete . $champs;
+} else {
+	$requete = $requete . " temperatureEau,temperature, humidity, date, sonMin, sonMax, sonMoy, co2, lumiere  ";
+}
+$requete = $requete . " FROM indicateur Order by date ";
+
+//LIMIT 10 OFFSET 5;  # Retourne les enregistrements 6 à 15
+if (!empty($limit)) {
+	$requete = $requete . " LIMIT $limit ";
+}
+if (!empty($offset)) {
+	$requete = $requete . " OFFSET $offset ";
+}
+
+//on execute la requete
+//print($requete);
+$sth =  mysql_query($requete);
+
+$rows = array();
+while($r = mysql_fetch_assoc($sth)) {
+    $rows[] = $r;
+}
+//print_r($rows);
+//On format le resultat. Soit tous les champs separée par une virgule, soit tous le flux en json
+if (!empty($champ)) {
+	//foreach($rows as $element)
+	//On affiche le permier element pour pas avoir a gerer le ',' en fin de ligne
+	$values = array();
+	$n =count($rows);
+	for ($numero = 0; $numero < $n; $numero++)
+	{
+		array_push($values, intval($rows[$numero][$champ]));
+	}
+	echo json_encode($values);
+} else {
+	print json_encode($rows);
+}
+
+// on ferme la connexion à mysql 
+mysql_close(); 
+?> 
