9128534a852de5a0b13807bf74cf8dd8c0ecf6db
xmoulin <xavier.moulin@gmail.com>
2013-12-10 22:36:14 +0100

    ajout des gauges lumiere, son, humidité

--- www/web/js/main.js
+++ www/web/js/main.js
@@ -202,7 +202,7 @@ function majJauge(data){
 	    		rotation: 'auto'
 	    	},
 	    	title: {
-	    		text: 'km/h'
+	    		text: '°C'
 	    	},
 	    	plotBands: [
 	    	{
@@ -237,4 +237,303 @@ function majJauge(data){
 	    }]
 
 	});
+
+$('#jaugeSon').highcharts({
+
+        chart: {
+            type: 'gauge',
+            plotBackgroundColor: null,
+            plotBackgroundImage: null,
+            plotBorderWidth: 0,
+            plotShadow: false
+        },
+
+        title: {
+            text: 'Sonomètre'
+        },
+
+        pane: {
+            startAngle: -150,
+            endAngle: 150,
+            background: [{
+                backgroundColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                    stops: [
+                    [0, '#FFF'],
+                    [1, '#333']
+                    ]
+                },
+                borderWidth: 0,
+                outerRadius: '109%'
+            }, {
+                backgroundColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                    stops: [
+                    [0, '#333'],
+                    [1, '#FFF']
+                    ]
+                },
+                borderWidth: 1,
+                outerRadius: '107%'
+            }, {
+                // default background
+            }, {
+                backgroundColor: '#DDD',
+                borderWidth: 0,
+                outerRadius: '105%',
+                innerRadius: '103%'
+            }]
+        },
+
+        // the value axis
+        yAxis: {
+            min: 0,
+            max: 80,
+
+            minorTickInterval: 'auto',
+            minorTickWidth: 1,
+            minorTickLength: 10,
+            minorTickPosition: 'inside',
+            minorTickColor: '#666',
+
+            tickPixelInterval: 50,
+            tickWidth: 2,
+            tickPosition: 'inside',
+            tickLength: 10,
+            tickColor: '#666',
+            labels: {
+                step: 2,
+                rotation: 'auto'
+            },
+            title: {
+                text: 'Db'
+            },
+            plotBands: [
+            {
+                from: 0,
+                to: 45,
+                color: '#55BF3B' // green
+            }, {
+                from: 45,
+                to: 55,
+                color: '#DDDF0D' // yellow
+            }, {
+                from: 55,
+                to: 80,
+                color: '#DF5353' // red
+            }]        
+        },
+
+        series: [{
+            name: 'Son',
+            data: data.moyenneSon,
+            tooltip: {
+                valueSuffix: ' Db'
+            }
+        }]
+
+    });
+
+
+$('#jaugeHumidite').highcharts({
+
+        chart: {
+            type: 'gauge',
+            plotBackgroundColor: null,
+            plotBackgroundImage: null,
+            plotBorderWidth: 0,
+            plotShadow: false
+        },
+
+        title: {
+            text: 'Humidité'
+        },
+
+        pane: {
+            startAngle: -150,
+            endAngle: 150,
+            background: [{
+                backgroundColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                    stops: [
+                    [0, '#FFF'],
+                    [1, '#333']
+                    ]
+                },
+                borderWidth: 0,
+                outerRadius: '109%'
+            }, {
+                backgroundColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                    stops: [
+                    [0, '#333'],
+                    [1, '#FFF']
+                    ]
+                },
+                borderWidth: 1,
+                outerRadius: '107%'
+            }, {
+                // default background
+            }, {
+                backgroundColor: '#DDD',
+                borderWidth: 0,
+                outerRadius: '105%',
+                innerRadius: '103%'
+            }]
+        },
+
+        // the value axis
+        yAxis: {
+            min: 10,
+            max: 90,
+
+            minorTickInterval: 'auto',
+            minorTickWidth: 1,
+            minorTickLength: 10,
+            minorTickPosition: 'inside',
+            minorTickColor: '#666',
+
+            tickPixelInterval: 50,
+            tickWidth: 2,
+            tickPosition: 'inside',
+            tickLength: 10,
+            tickColor: '#666',
+            labels: {
+                step: 2,
+                rotation: 'auto'
+            },
+            title: {
+                text: '%'
+            },
+            plotBands: [
+            {
+                from: 10,
+                to: 30,
+                color: '#DF5353' // red
+            },  {
+                from: 30,
+                to: 40,
+                color: '#DDDF0D' // yellow
+            }, {
+                from: 40,
+                to: 60,
+                color: '#55BF3B' // green
+            }, {
+                from: 60,
+                to: 70,
+                color: '#DDDF0D' // yellow
+            }, {
+                from: 70,
+                to: 90,
+                color: '#DF5353' // red
+            }]       
+        },
+
+        series: [{
+            name: 'Humidité',
+            data: data.humidity,
+            tooltip: {
+                valueSuffix: ' %'
+            }
+        }]
+
+    });
+
+
+$('#jaugeLumiere').highcharts({
+
+        chart: {
+            type: 'gauge',
+            plotBackgroundColor: null,
+            plotBackgroundImage: null,
+            plotBorderWidth: 0,
+            plotShadow: false
+        },
+
+        title: {
+            text: 'Lumière'
+        },
+
+        pane: {
+            startAngle: -150,
+            endAngle: 150,
+            background: [{
+                backgroundColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                    stops: [
+                    [0, '#FFF'],
+                    [1, '#333']
+                    ]
+                },
+                borderWidth: 0,
+                outerRadius: '109%'
+            }, {
+                backgroundColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                    stops: [
+                    [0, '#333'],
+                    [1, '#FFF']
+                    ]
+                },
+                borderWidth: 1,
+                outerRadius: '107%'
+            }, {
+                // default background
+            }, {
+                backgroundColor: '#DDD',
+                borderWidth: 0,
+                outerRadius: '105%',
+                innerRadius: '103%'
+            }]
+        },
+
+        // the value axis
+        yAxis: {
+            min: 0,
+            max: 200,
+
+            minorTickInterval: 'auto',
+            minorTickWidth: 1,
+            minorTickLength: 10,
+            minorTickPosition: 'inside',
+            minorTickColor: '#666',
+
+            tickPixelInterval: 50,
+            tickWidth: 2,
+            tickPosition: 'inside',
+            tickLength: 10,
+            tickColor: '#666',
+            labels: {
+                step: 2,
+                rotation: 'auto'
+            },
+            title: {
+                text: 'Lux'
+            },
+            plotBands: [
+            {
+                from: 120,
+                to: 200,
+                color: '#55BF3B' // green
+            }, {
+                from: 60,
+                to: 120,
+                color: '#DDDF0D' // yellow
+            }, {
+                from: 0,
+                to: 60,
+                color: '#DF5353' // red
+            }]        
+        },
+
+        series: [{
+            name: 'Lumiere',
+            data: data.lumiere,
+            tooltip: {
+                valueSuffix: ' Lux'
+            }
+        }]
+
+    });
+
 }
