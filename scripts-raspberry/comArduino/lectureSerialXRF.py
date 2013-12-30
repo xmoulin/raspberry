beef5a93761bb8573ac7d8f6b04a68368cfce543
xmoulin <xavier.moulin@gmail.com>
2013-12-30 17:18:32 +0100

    ca marche!

--- scripts-raspberry/comArduino/lectureSerialXRF.py
+++ scripts-raspberry/comArduino/lectureSerialXRF.py
@@ -1,11 +1,37 @@
 import serial
 import json
+from datetime import datetime
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost", # your host, usually localhost
+                     user="pi", # your username
+                      passwd="raspberry", # your password
+                      db="monitoring") # name of the data base
+
+# you must create a Cursor object. It will let
+#  you execute all the query you need
+cur = db.cursor() 
+
+
+def xmnData(donnees):
+     if 'iteration' in donnees:
+	cur.execute('INSERT INTO indicateur (iteration, temperature, humidity, date, sonMin, sonMax, sonMoy, gaz, lumiere) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)', (donnees['iteration'], donnees['temperature'], donnees['humidity'],datetime.now().isoformat(),donnees['sonMin'], donnees['sonMax'], donnees['sonMoy'], donnees['gaz'], donnees['lumiere']))
+#	cur.execute('INSERT INTO indicateur (iteration, temperatureEau, temperature, humidity) VALUES (%s,%s,%s,%s)', (donnees['iteration'], donnees['temperatureEau'], donnees['temperature'], donnees['humidity']))
+#	cur.execute('INSERT INTO indicateur (iteration, temperatureEau, temperature, humidity) VALUES (%s,%s,%s,%s)', (donnees['iteration'], 19.5523, 21.0, 50.0))
+#	cur.execute("INSERT INTO indicateur (iteration, temperatureEau, temperature, humidity) VALUES (1, 19.5523, 21.0, 50.0)")
+#	cur.execute('INSERT INTO indicateur (iteration, temperatureEau, temperature, humidity, date ) VALUES (%s,%s,%s,%s,%s)',(donnees['iteration'],donnees['temperatureEau'],donnees['temperature'],donnees['humidity'],1);
+	db.commit()
+        return donnees['temperature'] 
+     return donnees
 
 print("debut")
+#ser = serial.Serial('/dev/ttyACM0', 9600)
 ser = serial.Serial('/dev/ttyAMA0',9600)
-
 while 1 :
-        line = ser.readline().rstrip()
-        print(line)
-# print(ser.read(4))
-#	print(ser.readline().rstrip())
+	line = ser.readline().rstrip()
+	print(line)
+	if (line.startswith('{')): 
+		print('on a du json sur la ligne ci-dessus')
+		print(json.loads(line,object_hook=xmnData))
+#		print(xmnData(line))	
+
